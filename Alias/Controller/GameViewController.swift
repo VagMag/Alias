a2549f9f09ba6b51cee633656f66ce534c66ff15
i9orila <i9orila@aol.com>
2022-05-04 18:24:32 +0300

    icon and sound true skip

--- Alias/Controller/GameViewController.swift
+++ Alias/Controller/GameViewController.swift
@@ -6,24 +6,31 @@
 //
 
 import UIKit
+import AVFoundation
 
 class GameViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    @IBAction func TrueButtonPressed(_ sender: UIButton) {
+        playSound(soundName: "TRUE")
+    }
+    @IBAction func SkipButtonPressed(_ sender: UIButton) {
+        playSound(soundName: "FALSE")
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func playSound(soundName:String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "mp3")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+        
     }
-    */
 
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+}
+    
+
+    
 }
