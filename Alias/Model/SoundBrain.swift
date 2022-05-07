ba56542b6741af59b996949ea26901f9a0d993f9
kudr1kk22 <kudr1kk22@gmail.com>
2022-05-07 14:38:19 +0300

    add sound for buttons

---
+++ Alias/Model/SoundBrain.swift
@@ -0,0 +1,30 @@
+//
+//  SoundBrain.swift
+//  Alias
+//
+//  Created by kudr7k on 7.05.22.
+//
+
+import AVFoundation
+
+
+final class SoundBrain {
+    
+    var audio:AVPlayer!
+    
+    func answerRightSound() {
+            playSound(url: "rightSound")
+           
+        }
+
+        func skipWordSound() {
+            playSound(url: "skipWordSound")
+            
+        }
+        
+            func playSound(url: String) {
+            let url = Bundle.main.url(forResource: url, withExtension: "mp3")
+            audio = AVPlayer.init(url: url!)
+                audio.play()
+        }
+    }
